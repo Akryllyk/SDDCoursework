ec4f0613a7ac7ec467d9566507bd2e52cfdfa461
Alex Mayes <arvmayes@einet.brad.ac.uk>
2018-04-23 14:23:32 +0100

    added files and specification to folder

---
+++ src/SearchEngine/DatabaseHandler.java
@@ -0,0 +1,32 @@
+package SearchEngine;
+
+
+import java.sql.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author arvmayes
+ */
+public class DatabaseHandler {
+
+    public Connection handleDbConnection() {
+        Connection con = null;
+        try {
+            String username = "arvmayes";
+            String password = "testtest";
+            Class.forName("com.mysql.jdbc.Driver");
+
+            con = DriverManager.getConnection("jdbc:mysql://lamp.scim.brad.ac.uk:3306/arvmayes", username, password);
+        } catch (ClassNotFoundException cnfe) {
+            System.out.println(cnfe.getMessage());
+        } catch (SQLException se) {
+            System.out.println(se.getMessage());
+        }
+        return con;
+    }
+}
--- src/SearchEngine/GUIHandler.java
+++ src/SearchEngine/GUIHandler.java
@@ -1,3 +1,5 @@
+package SearchEngine;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,9 +10,14 @@
  *
  * @author arvmayes
  */
-public class MainRunnable {
-    public static void main(String[] args) {
-        //test
+import java.awt.event.*;
+import javax.swing.*;
+
+public class GUIHandler extends JFrame implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
-  
+    
 }
