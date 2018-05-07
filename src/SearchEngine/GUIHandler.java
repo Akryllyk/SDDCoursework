36ff02d2663f82c043b8babadd5117da28d606ec
Akryllyk <arvmayes@gmail.com>
2018-05-07 12:36:37 +0100

    added search bars and fixed radio buttons.

--- src/SearchEngine/GUIHandler.java
+++ src/SearchEngine/GUIHandler.java
@@ -14,7 +14,12 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class GUIHandler extends JFrame implements ActionListener {
-
+    //global variables
+    JPanel searchTerms = new JPanel();
+    JPanel titleBar = new JPanel();
+    JPanel searchBar = new JPanel();
+    ButtonGroup choice = new ButtonGroup();
+    
     private GUIHandler getme() {
         return this;
     }
@@ -27,25 +32,46 @@ public class GUIHandler extends JFrame implements ActionListener {
         searchTerms.setLayout(new BoxLayout(searchTerms, BoxLayout.PAGE_AXIS));
         setSearchTerms();
         setTitleBar();
+        setSearchBar();
         getContentPane().add(titleBar, BorderLayout.NORTH);
         getContentPane().add(searchTerms, BorderLayout.WEST);
+        getContentPane().add(searchBar, BorderLayout.CENTER);
         
     }
-    JPanel searchTerms = new JPanel();
-    JPanel titleBar = new JPanel();
+
 
     public void setSearchTerms() {
         JRadioButton but1 = new JRadioButton("Long/Lat");
         searchTerms.add(but1);
+        choice.add(but1);
         JRadioButton but2 = new JRadioButton("LSOA");
         searchTerms.add(but2);
+        choice.add(but2);
         JRadioButton but3 = new JRadioButton("Crime");
         searchTerms.add(but3);
+        choice.add(but3);
     }
 
     public void setTitleBar(){
         JLabel title = new JLabel("Crime Stats Search Engine");
         titleBar.add(title);
+    }
+    
+    public void setSearchBar(){
+        JLabel searchBar1Label = new JLabel();
+        JTextField search = new JTextField();
+        search.setColumns(50);
+        searchBar.add(searchBar1Label);
+        searchBar.add(search);
+        JLabel searchBar2Label = new JLabel();
+        JTextField search2 = new JTextField();
+        search2.setColumns(50);
+        searchBar.add(searchBar2Label);
+        searchBar.add(search2);
+        search.setVisible(false);
+        search2.setVisible(false);
+        
+        
     }
     private GUIImplementation gui = new GUIImplementation();
 
